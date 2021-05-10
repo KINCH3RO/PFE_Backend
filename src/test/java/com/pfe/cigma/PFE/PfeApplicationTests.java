babcc93b9adf8b2000023c9a6367453d1ed8eae0
KiNCH3RO <hat83203@gmail.com>
2021-05-10 13:56:11 +0000

    initial commit

---
+++ src/test/java/com/pfe/cigma/PFE/PfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfe.cigma.PFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
